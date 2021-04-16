ff63ab16e1c0bada883b7937b20f0ee093535206
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-16 18:13:28 -0500

    TRASLACION TERMINADA

--- mat.py
+++ mat.py
@@ -2,20 +2,23 @@ import numpy as np
 import os
 
 # ASIGNAR MEMORIA PARA LAS MATRICES Y VECTORES
-P = [0,0,0]
-R = [0,0,0]
-mat =  [[1,0,0,0],
-        [0,1,0,0],
-        [0,0,1,0],
-        [0,0,0,1]]
+P = np.array([0,0,0])
+R = np.array([0,0,0,1])
+mat = np.array([[1,0,0,0],
+                [0,1,0,0],
+                [0,0,1,0],
+                [0,0,0,1]])
 
-res =  [[0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0]]
+res = np.array([[0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0]])
 
 def rotacion():
-    # MOSTRAR MATRIZ
+    pass
+
+def traslacion():    
+        # MOSTRAR MATRIZ
     for i in range(len(mat)): # Filas
         for j in range(len(mat)): # Columnas
             res[i][j] = mat[i][j]
@@ -23,17 +26,31 @@ def rotacion():
     for i in res:
         print(i)
     '''
-    print("\n\n------LLENADO DE VECTORES------")
+    print("\n\n------LLENADO DE VECTOR P------")
     for i in range(len(P)):
         P[i] = int(input("Digite el valor {} del punto P: ".format(i)))
+    print("\n------LLENADO DE VECTOR R------")
+    for i in range(len(R)-1):
+        R[i] = int(input("Digite el valor {} del punto R: ".format(i)))
 
+    res[0][3] = P[0]
+    res[1][3] = P[1]
+    res[2][3] = P[2]
     
+    print("\n\n------MOSTRANDO MATRIZ------")
+    for i in res:
+        print(i)
 
-def traslacion():    
-    print("Digite los valores del punto P")
+    print("\n\n-----MOSTRANDO VECTOR-------")
+    for i in R:
+        print("["+str(i)+"]")
 
+    print("\n\n-----MOSTRANDO RESULTADO-------")
+    a = res.dot(R)
+    for i in range(len(a)):
+        print(a[i])
 
-
+            
 # SI ES ROTACIÓN O TRASLACIÓN
 os.system("clear")
 f = input("Resolver para rotación o traslacion? (R/T): ")
