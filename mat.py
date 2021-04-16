21dc3347d3f09774daa09d11dc623ae7a013f079
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-16 17:41:52 -0500

    pedir datos

--- mat.py
+++ mat.py
@@ -1,11 +1,45 @@
 import numpy as np
+import os
+
+# ASIGNAR MEMORIA PARA LAS MATRICES Y VECTORES
+P = [0,0,0]
+R = [0,0,0]
+mat =  [[1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,0],
+        [0,0,0,1]]
+
+res =  [[0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0]]
+
+def rotacion():
+    # MOSTRAR MATRIZ
+    for i in range(len(mat)): # Filas
+        for j in range(len(mat)): # Columnas
+            res[i][j] = mat[i][j]
+    '''
+    for i in res:
+        print(i)
+    '''
+    
+
+
+def traslacion():    
+    print("Digite los valores del punto P")
+
+
 
 # SI ES ROTACIÓN O TRASLACIÓN
+os.system("clear")
 f = input("Resolver para rotación o traslacion? (R/T): ")
 
 if(f == 'r' or f == 'R'):
-    print("ROTACIÓN")
+    rotacion()
 elif (f == 'T' or f == 't'):
-    print("TRASLACIÓN")
+    traslacion()
 else:
-    print("DIGITE NUMERO VALIDO")
+    print("DIGITO INCORRECTO")
+
+
