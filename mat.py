cd408ae44b8886f9a067b8edd67b7c23f9381571
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-16 20:24:49 -0500

    ROTACION -TERMINADO-

--- mat.py
+++ mat.py
@@ -14,15 +14,88 @@ res = np.array([[0,0,0,0],
                 [0,0,0,0],
                 [0,0,0,0]])
 
-
+######################################################################
 def tx():
-    print("TX")
+    print("\n\n------GRADOS DEL GIRO------")
+    grados = int(input("Que grados tendrá el giro: "))
+    print("\n\n------LLENADO DE VECTOR R------")
+    for i in range(len(R)-1):
+        R[i] = float(input("Digite el valor {} del punto R: ".format(i)))
+
+    mat[1][1] = round(np.cos(grados))
+    mat[1][2] = round(-np.sin(grados))
+    mat[2][1] = round(np.sin(grados))
+    mat[2][2] = round(np.cos(grados))
+    
+    print("\n\n------MOSTRANDO MATRIZ------")
+    for i in mat:
+        print(i)
+
+    print("\n\n-----MOSTRANDO VECTOR-------")
+    for i in R:
+        print("["+str(i)+"]")
+        
+    print("\n\n-----MOSTRANDO RESULTADO-------")
+    a = mat.dot(R)
+    for i in range(len(a)):
+        print(a[i])        
+        
+    
+######################################################################    
 def ty():
-    print("TY")
+    print("\n\n------GRADOS DEL GIRO------")
+    grados = int(input("Que grados tendrá el giro: "))
+    print("\n\n------LLENADO DE VECTOR R------")
+    for i in range(len(R)-1):
+        R[i] = float(input("Digite el valor {} del punto R: ".format(i)))
+
+    mat[0][0] = round(np.cos(grados))
+    mat[0][2] = round(np.sin(grados))
+    mat[2][0] = round(-np.sin(grados))
+    mat[2][2] = round(np.cos(grados))
+    
+    print("\n\n------MOSTRANDO MATRIZ------")
+    for i in mat:
+        print(i)
+    
+    print("\n\n-----MOSTRANDO VECTOR-------")
+    for i in R:
+        print("["+str(i)+"]")    
+    
+    print("\n\n-----MOSTRANDO RESULTADO-------")
+    a = mat.dot(R)
+    for i in range(len(a)):
+        print(a[i])
+        
+        
+######################################################################    
 def tz():
-    print("TZ")
+    print("\n\n------GRADOS DEL GIRO------")
+    grados = int(input("Que grados tendrá el giro: "))
+    print("\n\n------LLENADO DE VECTOR R------")
+    for i in range(len(R)-1):
+        R[i] = float(input("Digite el valor {} del punto R: ".format(i)))
 
+    mat[0][0] = round(np.cos(grados))
+    mat[0][1] = round(-np.sin(grados))
+    mat[1][0] = round(np.sin(grados))
+    mat[1][1] = round(np.cos(grados))
+    
+    print("\n\n------MOSTRANDO MATRIZ------")
+    for i in mat:
+        print(i)
 
+    print("\n\n-----MOSTRANDO VECTOR-------")
+    for i in R:
+        print("["+str(i)+"]")        
+        
+    print("\n\n-----MOSTRANDO RESULTADO-------")
+    a = mat.dot(R)
+    for i in range(len(a)):
+        print(a[i])
+        
+        
+######################################################################
 def rotacion():
     sentido = input("Eje en que rotará (X, Y, Z): ")
     if(sentido == 'X' or sentido == 'x'):
@@ -34,7 +107,7 @@ def rotacion():
     else:
         print("DIGITO INCORRECTO")  
         
-        
+######################################################################  
 def traslacion():    
         # MOSTRAR MATRIZ
     for i in range(len(mat)): # Filas
@@ -46,10 +119,10 @@ def traslacion():
     '''
     print("\n\n------LLENADO DE VECTOR P------")
     for i in range(len(P)):
-        P[i] = int(input("Digite el valor {} del punto P: ".format(i)))
+        P[i] = float(input("Digite el valor {} del punto P: ".format(i)))
     print("\n------LLENADO DE VECTOR R------")
     for i in range(len(R)-1):
-        R[i] = int(input("Digite el valor {} del punto R: ".format(i)))
+        R[i] = float(input("Digite el valor {} del punto R: ".format(i)))
 
     res[0][3] = P[0]
     res[1][3] = P[1]
@@ -68,15 +141,29 @@ def traslacion():
     for i in range(len(a)):
         print(a[i])
 
-            
+######################################################################  
+def rot_tras():
+    pass
+
+######################################################################
+def tras_rot():
+    
+    
+    pass        
+        
+######################################################################        
 # SI ES ROTACIÓN O TRASLACIÓN
 os.system("clear")
-f = input("Resolver para rotación o traslacion? (R/T): ")
+f = input("Resolver para rotación, traslacion, Rot_Tras, Tras_Ror? (R/T/RT/TR): ")
 
 if(f == 'r' or f == 'R'):
     rotacion()
 elif (f == 'T' or f == 't'):
     traslacion()
+elif (f == 'RT' or f == 'rt' or f == 'rT' or f == 'Rt'):
+    rot_tras()
+elif (f == 'TR' or f == 'tr' or f == 'Tr' or f == 'tR'):
+    tras_rot()
 else:
     print("DIGITO INCORRECTO")
 
